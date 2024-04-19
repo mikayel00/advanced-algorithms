ccdba4eb51cb70437fc4080f0de516a0458c4646
Mikayel00 <mikayel.hovhannisyan27@gmail.com>
2024-04-19 18:53:36 +0400

    add searchs

--- search/binary-search/binarySearch.js
+++ search/binary-search/binarySearch.js
@@ -1,13 +1,12 @@
-function search(arr, value) {
-    let min = 0;
-    let max = arr.length - 1;
-
-    while (min < max) {
-        let middle = Math.floor((min + max) / 2);
-        if (arr[middle] < value) {
-            min = middle + 1;
-        } else if (arr[middle] > value) {
-            max = middle - 1;
+function binarySearch(arr, value) {
+    let left = 0;
+    let right = arr.length - 1;
+    while (left <= right) {
+        let middle = Math.floor((left + right) / 2);
+        if (arr[middle] > value) {
+            right = middle - 1;
+        } else if (arr[middle] < value) {
+            left = middle + 1;
         } else {
             return middle;
         }
@@ -15,4 +14,20 @@ function search(arr, value) {
     return -1;
 }
 
-console.log(search([1,2,3,6,8,9,14,18,23,55,59,60,69,80], 15));
+console.log(binarySearch([1, 2, 3, 6, 8, 9, 14, 18, 23, 55, 59, 60, 69, 80], 18));
+console.log(binarySearch([1, 2, 3, 4, 5], 2)) // 1
+console.log(binarySearch([1, 2, 3, 4, 5], 3)) // 2
+console.log(binarySearch([1, 2, 3, 4, 5], 5)) // 4
+console.log(binarySearch([1, 2, 3, 4, 5], 6)) // -1
+console.log(binarySearch([
+    5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
+    40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+], 10)) // 2
+console.log(binarySearch([
+    5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
+    40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+], 95)) // 16
+console.log(binarySearch([
+    5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
+    40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+], 100)) // -1
